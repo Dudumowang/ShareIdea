9060a818172b8ef5d8842daebfadace9422f4f2e
Dudumowang <1658763909@qq.com>
2022-11-23 14:34:54 +0800

    UserDTO

--- src/main/java/com/example/springboot/controller/dto/UserPasswordDTO.java
+++ src/main/java/com/example/springboot/controller/dto/UserPasswordDTO.java
@@ -1,4 +1,31 @@
 package com.example.springboot.controller.dto;
 
 public class UserPasswordDTO {
+    private String id;
+    private String oldPassword;
+    private String newPassword;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
 }
